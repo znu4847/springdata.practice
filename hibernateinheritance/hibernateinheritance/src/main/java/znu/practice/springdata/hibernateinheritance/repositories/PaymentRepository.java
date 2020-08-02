a27de26418f352232bae48382b35a70a4349c056
JinWoo Kim <znu4847@gmail.com>
2020-08-02 23:25:03 +0900

    trying to preserve SINGLE_TABLE_STRATEGY

--- hibernateinheritance/hibernateinheritance/src/main/java/znu/practice/springdata/hibernateinheritance/repositories/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package znu.practice.springdata.hibernateinheritance.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import znu.practice.springdata.hibernateinheritance.entities.Payment;
-
-public interface PaymentRepository extends CrudRepository<Payment, Integer> {
-
-}
